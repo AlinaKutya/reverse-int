a015fc3670f51d92845a6e5dacd61ba7c19f1b06
Alina Kutyeva <flytta.dig@mail.ru>
2020-02-25 02:03:52 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nAbs = Math.abs(n);
+    let number = nAbs
+        .toString()
+        .split("")
+        .reverse()
+        .join("");
+    return number;
+};
